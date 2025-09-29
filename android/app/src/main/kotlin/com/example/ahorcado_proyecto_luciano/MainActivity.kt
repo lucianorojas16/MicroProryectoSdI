9c664cdd6a6e52ba3e6a9edab7c7971a8d789b75
lucianorojas16 <luciano.rojas@correo.unimet.edu.ve>
2025-09-29 10:46:13 -0400

    Primer commit

---
+++ android/app/src/main/kotlin/com/example/ahorcado_proyecto_luciano/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ahorcado_proyecto_luciano
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'game_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Juego del Ahorcado',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: const GameScreen(),
+    );
+  }
+}
